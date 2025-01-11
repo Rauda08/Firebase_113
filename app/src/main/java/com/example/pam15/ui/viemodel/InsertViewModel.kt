b69be39e598fa37e6f73734f4c04a29051e947eb
Rauda08 <raudajnnh08@gmail.com>
2025-01-11 22:57:12 +0700

    membuat class InsertViewModel

---
+++ app/src/main/java/com/example/pam15/ui/viemodel/InsertViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.pam15.ui.viemodel
+
+import androidx.lifecycle.ViewModel
+import com.example.pam15.repository.RepositoryMhs
+
+class InsertViewModel(
+    private val mhs: RepositoryMhs
+) : ViewModel() {
+
+}
