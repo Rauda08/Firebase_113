22e485d7d84646e36e2ffc8a7ff87cfbcb9d0d77
Rauda08 <raudajnnh08@gmail.com>
2025-01-12 01:07:52 +0700

    membuat function InsertMhsView di insert view

---
+++ app/src/main/java/com/example/pam15/ui/view/InsertView.kt
@@ -0,0 +1,83 @@
+package com.example.pam15.ui.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pam15.ui.viemodel.FormState
+import com.example.pam15.ui.viemodel.InsertViewModel
+import com.example.pam15.ui.viemodel.PenyediaViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@Composable
+fun InsertMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.uiState
+    val snackbarHostState = remember { SnackbarHostState() }// State utama untuk loading, success, error
+    val uiEvent = viewModel.uiEvent // State untuk form dan validasi val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+// Observasi perubahan state untuk snackbar dan navigasi
+    LaunchedEffect(uiState) {
+        when (uiState) {
+            is FormState.Success -> {
+                println(
+                "InsertMhsView: uiState is FormState.Success, navigate to home " + uiState.message
+            )
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message) // Tampilkan snackbar
+                }
+                delay(700)
+                onNavigate()
+                viewModel.resetSnackBarMessage() // Reset snackbar state
+            }
+
+            is FormState.Error -> {
+                coroutineScope.launch {
+                snackbarHostState.showSnackbar(uiState.message) }
+            }
+            else -> Unit
+        }
+    }
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            TopAppBar(
+                title = { Text("Tambah Mahasiswa") }, navigationIcon = {
+                    Button(onClick = onBack) { Text("Back")
+                    }
+                }
+            )
+        }
+    ) { padding -> Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(padding)
+            .padding(16.dp)
+    ) {
+
+    }
+
+
+
+
+
+
